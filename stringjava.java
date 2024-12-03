56e9bc6bcd8e73c359c98c34aa12ad6e3b66bd3e
Mohammad oli56 <olimohammad286@gmai.com>
2024-12-04 02:08:35 +0600

    Java String methods

--- stringjava.java
+++ stringjava.java
@@ -4,29 +4,29 @@ public class stringjava {
     public static void main(String[] args) {
       // 1. length method
 
-      //String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      //System.out.println("The length of the txt string is: " + txt.length());
+      String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      System.out.println("The length of the txt string is: " + txt.length());
 
 
 
       // 2.Uppercase and lowercase methods
 
-      // String txt = "Hello World";
-      // System.out.println(txt.toUpperCase());
-      // System.out.println(txt.toLowerCase());
+      String txta = "Hello World";
+      System.out.println(txt.toUpperCase());
+      System.out.println(txt.toLowerCase());
 
 
       // 3.Finding a Character in a String {indexof()}
 
-      // String txt = "Please locate where 'locate' occurs!";
-      //System.out.println(txt.indexOf("locate"));
+      String txte = "Please locate where 'locate' occurs!";
+      System.out.println(txt.indexOf("locate"));
 
       
       // 4.the concat() method to concatenate two strings
 
-      // String firstName = "John ";
-      // String lastName = "Doe";
-      // System.out.println(firstName.concat(lastName));
+      String firstName = "John ";
+      String lastName = "Doe";
+      System.out.println(firstName.concat(lastName));
 
 
       // 5. Add more Soon

d667c122e64f7c9370ddc17d09239ddcf8d7279f
Mohammad oli56 <olimohammad286@gmai.com>
2024-12-04 22:41:40 +0600

    Hello world

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package JAVA;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello worldcd");
+        System.out.println("Hello world !");
     }
 }

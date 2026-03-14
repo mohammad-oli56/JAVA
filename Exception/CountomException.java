3c8e8f3f18a56a7f1e9573aa2c2ac4a460677572
mohammad-oli <165128206+mohammad-oli56@users.noreply.github.com>
2026-03-14 16:10:43 +0600

    Update CountomException.java
    creat own exception

--- Exception/CountomException.java
+++ Exception/CountomException.java
@@ -1,1 +1,26 @@
+package coustonException;
+
+import java.util.Scanner;
+
+public class coustomException {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        try {
+            if(age>100){
+                throw new  Myexception("My Exception message");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
+
+class Myexception extends Exception{
+    public Myexception(String message) {
+        super(message);
+    }
+}
 
